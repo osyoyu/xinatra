c8e4554801224dff8804242cc6b818eb2dc61648
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-12-26 22:44:38 +0900

    Implement get

--- lib/xinatra/base.rb
+++ lib/xinatra/base.rb
@@ -2,9 +2,18 @@
 
 module Xinatra
   class Base
+    @@routes = {}
+
+    class << self
+      def get(path, &block)
+        @@routes[path] = block
+      end
+    end
+
     def call(env)
-      if env['REQUEST_METHOD'] == 'GET' && env['PATH_INFO'] == '/'
-        [200, { 'Content-Type' => 'text/plain' }, ['hello']]
+      if env['REQUEST_METHOD'] == 'GET' && handler = @@routes[env['PATH_INFO']]
+        retstr = handler.call
+        [200, { 'Content-Type' => 'text/plain' }, [retstr]]
       else
         [404, { 'Content-Type' => 'text/plain' }, ['']]
       end
