7c9689f28ed4fc796c932ff01bd1dca7a0d184b2
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-12-29 01:23:03 +0900

    Implement LinearRouter, TrieRouter

--- lib/xinatra.rb
+++ lib/xinatra.rb
@@ -2,6 +2,8 @@
 
 require_relative "xinatra/version"
 require_relative "xinatra/base"
+require_relative "xinatra/linear_router"
+require_relative "xinatra/trie_router"
 
 module Xinatra
   class Error < StandardError; end
